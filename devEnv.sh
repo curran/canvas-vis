0f809946bd106ea0b4804ba2d0e543971aaf1cf0
curran <curran.kelleher@gmail.com>
2013-04-09 23:32:46 -0400

    Added installation comments to devEnv

--- devEnv.sh
+++ devEnv.sh
@@ -6,6 +6,15 @@
 #   * git
 #   * Node.js
 #
+# To install the requisite stuff in Ubuntu:
+#
+#   * `sudo apt-get install git -y`
+#   * `sudo apt-get install python-software-properties python g++ make -y` ([source](https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager#ubuntu))
+#   * `sudo apt-get install software-properties-common -y`
+#   * `sudo add-apt-repository ppa:chris-lea/node.js`
+#   * `sudo apt-get update`
+#   * `sudo apt-get install nodejs -y`
+#
 # Run this script once and leave it running in a visible terminal:
 #
 #   * Serves the `canvas-vis` directory tree at [http://localhost:8080](http://localhost:8080)
